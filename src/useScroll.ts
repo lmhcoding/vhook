70b2a3a1b2debc0e489ee9dca7b2f950326ba64b
lmhcoding <13265878974@163.com>
2020-09-19 16:07:30 +0800

    feat(useScroll): add useScroll

---
+++ src/useScroll.ts
@@ -0,0 +1,29 @@
+import { reactive, toRefs, ToRefs } from 'vue'
+import { useEvent, Target } from './useEvent'
+
+export type ScrollState = ToRefs<{
+  x: number
+  y: number
+}>
+
+export function useScroll(target: Target): ScrollState {
+  if (target === window && process.env.NODE_ENV !== 'production') {
+    throw Error('target of useScroll should not be window')
+  }
+  const state = reactive({
+    x: 0,
+    y: 0
+  })
+  const eventTarget = useEvent(
+    'scroll',
+    () => {
+      if (eventTarget.value) {
+        state.x = ((eventTarget.value as unknown) as Element).scrollLeft
+        state.y = ((eventTarget.value as unknown) as Element).scrollTop
+      }
+    },
+    { capture: false, passive: true },
+    target
+  )
+  return toRefs(state)
+}
